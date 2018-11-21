5af865eb278e821e634cf1cc3cb3ea2c8a1e56bc
Kamil Mingulov <kmingulov@de.egr.lan>
2018-11-21 16:04:44 +0100

    21.11 chair+textures

--- configurator.js
+++ configurator.js
@@ -9,11 +9,8 @@ var MyConfigurator = /** @class */ (function () {
     MyConfigurator.prototype.createScene = function () {
         this._scene = new BABYLON.Scene(this._engine);
         this._camera = new BABYLON.ArcRotateCamera("camera1", 0, 0, 10, BABYLON.Vector3.Zero(), this._scene);
-        this._camera.attachControl(this._canvas, false);
+        //this._camera.attachControl(this._canvas,false);
         this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this._scene);
-        var sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { segments: 16, diameter: 2 }, this._scene);
-        sphere.position.y = 1;
-        var ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 6, height: 6, subdivisions: 2 }, this._scene);
     };
     MyConfigurator.prototype.doRender = function () {
         var _this = this;
@@ -25,13 +22,54 @@ var MyConfigurator = /** @class */ (function () {
     MyConfigurator.prototype.getScene = function () {
         return this._scene;
     };
+    MyConfigurator.prototype.getEngine = function () {
+        return this._engine;
+    };
     return MyConfigurator;
 }());
-window.addEventListener('DOMContentLoaded', function () {
-    var configurator = new MyConfigurator('renderCanvas');
-    configurator.createScene();
-    BABYLON.SceneLoader.ImportMesh("", "", "interior.obj", configurator.getScene(), function (object) {
-        console.log(object);
-    });
+//let configurator=new MyConfigurator('renderCanvas');
+var canvas = document.getElementById('renderCanvas');
+var engine = new BABYLON.Engine(canvas, true, { stencil: true });
+var scene;
+var camera;
+var light;
+var hl;
+var index = 1;
+var loader = BABYLON.SceneLoader.Load("", "Office_chair.obj", engine, function (newScene) {
+    console.log(newScene);
+    scene = newScene;
+    hl = new BABYLON.HighlightLayer("highlight", scene);
+    //scene.useRightHandedSystem=true;
+    var mesh = scene.meshes[0];
+    mesh.rotate(BABYLON.Axis.X, -Math.PI / 2);
+    //var mesh=<BABYLON.Mesh>scene.meshes[0]
+    //hl.addMesh(mesh,BABYLON.Color3.Blue());
+    mesh.scaling.x = 0.01;
+    mesh.scaling.y = 0.01;
+    mesh.scaling.z = 0.01;
+    var material = new BABYLON.StandardMaterial("mat", scene);
+    // material.diffuseColor = BABYLON.Color3.Green();
+    material.bumpTexture = new BABYLON.Texture("C:/Users/kmingulov/Documents/Configurator/Office_chair_plastic_normal.png", scene);
+    material.diffuseTexture = new BABYLON.Texture("C:/Users/kmingulov/Documents/Configurator/Office_chair_plastic_dif.png", scene);
+    var material2 = new BABYLON.StandardMaterial("mat2", scene);
+    // material2.diffuseColor=BABYLON.Color3.Blue();
+    material2.diffuseTexture = new BABYLON.Texture("C:/Users/kmingulov/Documents/Configurator/Office_chair_fabric_dif.png", scene);
+    material2.bumpTexture = new BABYLON.Texture("C:/Users/kmingulov/Documents/Configurator/Office_chair_fabric_normal.png", scene);
+    //mesh.material=material;
+    var multimat = new BABYLON.MultiMaterial("mumat", scene);
+    multimat.subMaterials.push(material);
+    multimat.subMaterials.push(material2);
+    var vertcount = mesh.getTotalVertices();
+    mesh.subMeshes.push(new BABYLON.SubMesh(0, 0, vertcount, 0, 0, mesh));
+    mesh.subMeshes.push(new BABYLON.SubMesh(1, 0, vertcount, 0, 10650, mesh));
+    mesh.material = multimat;
+    //scene.addEffectLayer(hl);
+    light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    camera = new BABYLON.FreeCamera("camera", BABYLON.Vector3.Zero(), scene);
+    camera.attachControl(canvas, true);
+    camera.position.y = 10;
+    camera.setTarget(BABYLON.Vector3.Zero());
+    engine.runRenderLoop(function () { scene.render(); });
+    scene.addLight(light);
 });
 //# sourceMappingURL=configurator.js.map
